5067187155a355cbc3e74732beb832fd4f1e6d64
Maksym Malynovskyi <max@Maksyms-MacBook-Air.local>
2022-11-04 16:52:50 +0200

    Refactor

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
 'use strict';
 
-$(function(){
-    const BALL_COLOR = '#00FF00';
-    // canvas initialization
-    let canvas = $('#canvas')[0];
-    let context = canvas.getContext('2d');
+const PIXELS_IN_METER = 100;
 
-    $('#startButton').click(function() {
-        $('#benchmark').hide();
-        // coefficient, how many speed saves the ball, when hitting the floor
-        let speedConservationCoefficient = parseFloat($('#speed-conservation-coefficient').val());
+// meters (float or integer)
+const MIN_DEBOUNCE_HEIGHT = 0.01;
+
+const BALL_COLOR = '#00FF00';
+
+addEventListener('DOMContentLoaded', () => {
+    // canvas initialization
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const benchmark = document.getElementById('benchmark');
+
+    document.getElementById('startButton').onclick = () => {
+        benchmark.style.visibility = 'hidden';
+
+        // defines what fraction of initial speed is left after hitting the surface
+        const speedConservationCoefficient = parseFloat(document.getElementById('speed-conservation-coefficient').value);
         // m/s^2
-        let acceleration = parseFloat($('#g').val());
-        // m
-        let height = parseFloat($('#height').val());
+        const g = parseFloat(document.getElementById('g').value);
+        // meters
+        const height = parseFloat(document.getElementById('height').value);
         // px
-        let radius = parseInt($('#radius').val());
-        let diameter = radius*2;
+        const radius = parseInt(document.getElementById('radius').value);
+        const diameter = radius*2;
+
+        // position surface
+        document.getElementById('surface').style.top = `${diameter + (height * PIXELS_IN_METER)}px`;
 
         // clear
         canvas.height = canvas.width = diameter;
@@ -27,20 +38,137 @@ $(function(){
         context.fillStyle = BALL_COLOR;
         context.fill();
 
-        // move ball to starting position
+        // move ball to the starting position
         canvas.style.top = '0';
 
-        animate.call(this, {
+        animate({
             canvas: canvas,
             speedConservationCoefficient: speedConservationCoefficient,
-            acceleration: acceleration,
+            g: g,
             height: height
         });
-    }.bind(this));
+    };
+});
 
-    $(this).on('ballStopped', function() {
-        $('#benchmark').show();
-        $('#elapsed-time').text(Math.round(window.elapsedTime) + ' s elapsed');
-        $('#fps').text(Math.round(window.fps) + ' FPS');
-    });
-}.bind(this));
+addEventListener('ballStopped', (e) => {
+    document.getElementById('benchmark').style.visibility = 'visible';
+    document.getElementById('elapsed-time').innerText = `${Math.round(e.elapsedTime)} s elapsed`;
+    document.getElementById('fps').innerText = `${Math.round(e.frameCount / e.elapsedTime)} fps`;
+    document.getElementById('frame-count').innerText = `${e.frameCount} frame(s) rendered`;
+});
+
+const debounceHeight = (velocity, g) => { return Math.pow(velocity, 2) / (2 * g) };
+
+const canDebounce = (velocity, g) => { return debounceHeight(velocity, g) > MIN_DEBOUNCE_HEIGHT };
+
+const calculateBounceParams = (options) => {
+    let bounceCount = 0;
+    let bounceParams = [
+        {
+            // surface level
+            targetPoint: options.height * PIXELS_IN_METER,
+            // m/s (when falling first time)
+            speedNearTheFloor: Math.sqrt(2 * options.g * options.height)
+        }
+    ];
+    // while jump height greater than MIN_DEBOUNCE_HEIGHT - bouncing
+    let deboundeInitialSpeed = bounceParams[bounceCount].speedNearTheFloor * options.speedConservationCoefficient;
+    let bounceHeight, targetPoint, speedDown;
+    while (canDebounce(deboundeInitialSpeed, options.g)) {
+        // decrease the speed according to speed conservation coefficient
+        deboundeInitialSpeed = bounceParams[bounceCount].speedNearTheFloor * options.speedConservationCoefficient;
+        bounceHeight = Math.pow(deboundeInitialSpeed, 2) / (2 * options.g);
+        targetPoint = Math.floor(bounceParams[0].targetPoint - (bounceHeight * PIXELS_IN_METER));
+        speedDown = Math.sqrt(2 * options.g * bounceHeight);
+        // every bounce after first drop is a jump and and subsequent fall
+        bounceParams.push(
+            {
+                targetPoint: targetPoint
+            }
+        );
+        bounceParams.push(
+            {
+                speedNearTheFloor: speedDown
+            }
+        );
+        bounceCount = bounceParams.length - 1;
+    }
+    return bounceParams;
+}
+
+const animationStep = (now, options) => {
+    let h = 0;
+    let pos = 0;
+
+    // falling
+    if (!(options.step % 2) || options.step === 0) {
+        // path that the ball has passed in meters ( h = at^2 / 2 )
+        h = (options.g * Math.pow((now - options.directionChangedTime) / 1000, 2)) / 2;
+        pos = Math.floor(parseInt(options.canvas.style.top) + (h * PIXELS_IN_METER));
+        // Correct possible passing surface layer because of low js ticker resolution
+        if (pos > options.bounceParams[0].targetPoint) {
+            pos = options.bounceParams[0].targetPoint;
+        }
+        // check if the ball has touched the surface
+        if (pos === options.bounceParams[0].targetPoint) {
+            options.changeDirection(now);
+        }
+    // debouncing
+    } else {
+        // initial ball debounce speed
+        const v0 = options.bounceParams[options.step - 1].speedNearTheFloor;
+        // time ball going up since last frame
+        const t = (now - options.directionChangedTime) / 1000;
+        // ball speed at the end of the current time frame
+        const v1 = v0 - (options.g * t);
+        // path that has passed the ball (meters)
+        h = (v1 * t) - ((options.g * Math.pow(t, 2)) / 2);
+        // ball position at the end of the current time frame
+        pos = Math.floor(parseInt(options.canvas.style.top) - (h * PIXELS_IN_METER));
+        // maybe the ball has already reached its targetPoint in the current debounce
+        if (pos <= options.bounceParams[options.step].targetPoint) {
+            pos = options.bounceParams[options.step].targetPoint;
+            options.changeDirection(now);
+        } else if (h < 0 && pos !== options.bounceParams[options.step].targetPoint) {
+            // ball has already flyed over top point and is actually falling
+            options.changeDirection(now);
+            pos =  options.bounceParams[options.step].targetPoint;
+        }
+    }
+
+    // animation end, place ball onto the surface
+    if (options.step == options.bounceParams.length) {
+        pos = options.bounceParams[0].targetPoint;
+        options.changeDirection(now);
+    }
+    options.canvas.style.top = pos + 'px';
+    ++options.frameCount;
+};
+
+const animate = (options) => {
+    options.directionChangedTime = performance.now();
+    options.changeDirection = (now) => {
+        // switch ball moving direction
+        ++options.step;
+        // direction switching time
+        options.directionChangedTime = now;
+    };
+    options.bounceParams = calculateBounceParams(options);
+    options.step = 0;
+    options.frameCount = 0;
+    options.startTime = Math.trunc(performance.now() / 1000);
+
+    const nextFrame = (now) => {
+        animationStep(now, options);
+        if (options.step <= options.bounceParams.length) {
+            requestAnimationFrame(nextFrame);
+        } else {
+            const e = new Event('ballStopped');
+            e.elapsedTime = Math.trunc(performance.now() / 1000) - options.startTime;
+            e.frameCount = options.frameCount;
+            window.dispatchEvent(e);
+        }
+    }
+
+    requestAnimationFrame(nextFrame);
+}
--- readme.md
+++ readme.md
@@ -1,19 +1,24 @@
 ## README
 
-[Demo video](https://youtu.be/QkeTCYBJYy8) (youtube, low fps)
+[Demo video on youtube](https://youtu.be/iaNiIlN-Ybo)
 
-It's a ball, that bounces accordingly to (almost) real-life physics, or to your own parameters.
+It's a ball, that bounces accordingly to (almost) real-life physics, or parameters of your choise.
 
-The main parameter of the ball's bouncing is a speed conservation coefficient. How does this coefficient work?
+The main parameter of the ball's bouncing is a speed conservation coefficient.
 
 Consider falling ball, that touches the floor.
 
-In that case, in real life, the ball a little deformed, respectively its elasticity, and other parameters.
-The surface on which it has fallen, is also have a little deformed.
-Part of the kinetic energy of the ball would be transformed into heat energy,
-some would reject a sound (if it is not a vacuum), then there would be a partial recovery
-of the original shape of the ball and the surface beneath it, causing the ball to bounce.
+When falling ball touches the surface in real life, the ball is being a little deformed respectively its elasticity and other parameters.
+The surface on which it has fallen, is also being a little deformed.
+Part of the kinetic energy of the ball would be transformed into heat.
+Some kinetic energy will be spent on creating waves of pressure in surrounding matter (sound
+electromagnetic and gravitational waves. At some point the ball and surrounding matter will partially
+recover their shapes and as a result the ball will bounce. This process in real life is almost unstoppable
+until some Planck time, Planck distance and Planck energy quantum effects make the ball actuall stop
+moving.
 
-Instead of doing this over-complicated calculations, I use a single coefficient.
-When ball hits the floor, I just multiply its speed near the floor by that coefficient,
-and use the resulting value as the initial vertical velocity of the ball.
+Instead of doing this over-complicated calculations (which requires a supercomputer),
+I use a single coefficient that defines how much speed that the ball has gained while
+falling is preserved when the ball is debouncing.
+If that initial debounce speed allows the ball to jump higher than 1px under condition of free fall
+acceleration pressing the ball to the surface - it will debounce. Otherwice it will stop.
